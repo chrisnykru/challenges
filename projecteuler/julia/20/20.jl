0dc5d111a32dac677d33d9dc555451c64ba41583
zeroshirts <zeroshirts.tmp@gmail.com>
2015-06-16 23:51:18 -0500

    p20

---
+++ projecteuler/julia/20/20.jl
@@ -0,0 +1,26 @@
+#=
+
+Factorial digit sum
+
+n! means n * (n  1) * ... * 3 * 2 * 1
+
+For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+=#
+
+using Base.Test
+
+function sumDigits()
+  x = factorial(big(100))
+  sum = 0
+  while x > 0
+    sum += mod(x, 10)
+	x = div(x, 10)
+  end
+  return sum
+end
+
+@time @test sumDigits() == 648
