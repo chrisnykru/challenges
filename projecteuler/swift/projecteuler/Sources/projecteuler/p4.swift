2bda9e64049af9b889d1ed093b9cf9500cf56162
cds <c@neosholab.com>
2020-09-18 22:14:13 -0500

    p4 done

--- projecteuler/swift/projecteuler/Sources/projecteuler/p4.swift
+++ projecteuler/swift/projecteuler/Sources/projecteuler/p4.swift
@@ -3,13 +3,15 @@
 Largest palindrome product
 
 A palindromic number reads the same both ways. The largest palindrome made
-from the product of two 2-digit numbers is 9009 = 91  99.
+from the product of two 2-digit numbers is 9009 = 91 * 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 
 */
 
-func isPalindrome(x: Int) -> Bool {
+import Darwin
+
+func isPalindrome(_ x: Int) -> Bool {
     let xstr = String(x)
     let x = xstr.unicodeScalars.map { $0.value }
     
@@ -23,10 +25,35 @@ func isPalindrome(x: Int) -> Bool {
     return true
 }
 
-struct PalindromeProduct {
+struct PalindromeProduct: Equatable {
     var i = 0
     var j = 0
     var product = 0
+}
+
+func findLargestPalindrome(_ numDigits: Int) -> PalindromeProduct {
+    var largest = PalindromeProduct(i: 0, j: 0, product: 0)
+    
+    let max = Int(pow(Double(10), Double(numDigits))) - 1
+    // remember, communitive property of multiplication
+    // don't test 9*8 and 8*9
+    var i = max
+    while i > 0 {
+        var j = i
+        while j > 0 {
+            let ij = i * j
+            if isPalindrome(ij) {
+                if ij > largest.product {
+                    largest.i = i
+                    largest.j = j
+                    largest.product = ij
+                }
+            }
+            j -= 1
+        }
+        i -= 1
+    }
+    return largest
 }
 
 
--- projecteuler/swift/projecteuler/Tests/projecteulerTests/p4Tests.swift
+++ projecteuler/swift/projecteuler/Tests/projecteulerTests/p4Tests.swift
@@ -3,14 +3,19 @@ import XCTest
 
 final class p4Tests: XCTestCase {
     func testIsPalindrome() {
-        XCTAssertEqual(isPalindrome(x: 422), false)
-        XCTAssertEqual(isPalindrome(x: 424), true)
-        XCTAssertEqual(isPalindrome(x: 42), false)
-        XCTAssertEqual(isPalindrome(x: 1221), true)
-        XCTAssertEqual(isPalindrome(x: 1222), false)
+        XCTAssertEqual(isPalindrome(422), false)
+        XCTAssertEqual(isPalindrome(424), true)
+        XCTAssertEqual(isPalindrome(42), false)
+        XCTAssertEqual(isPalindrome(1221), true)
+        XCTAssertEqual(isPalindrome(1222), false)
         
     }
+    func testFindLargestPalindrome() {
+        XCTAssertEqual(findLargestPalindrome(2), PalindromeProduct(i: 99, j: 91, product: 9009))
+        XCTAssertEqual(findLargestPalindrome(3), PalindromeProduct(i: 993, j: 913, product: 906609))
+    }
     static var allTests = [
-        ("testIsPalindrome", testIsPalindrome)
+        ("testIsPalindrome", testIsPalindrome),
+        ("testFindLargestPalindrome", testFindLargestPalindrome)
     ]
 }
