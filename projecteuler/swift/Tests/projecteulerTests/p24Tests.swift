a88a1457f80f6e993e7e1b70b1e3a8384b652567
cds <c@neosholab.com>
2020-12-16 00:05:01 -0700

    p24 in progress

--- projecteuler/swift/Tests/projecteulerTests/p24Tests.swift
+++ projecteuler/swift/Tests/projecteulerTests/p24Tests.swift
@@ -2,7 +2,72 @@ import XCTest
 @testable import projecteuler
 
 final class p24Tests: XCTestCase {
+    func testPermGenHelper(_ perm: [Int], _ expected: [[Int]]) throws {
+        var x = try PermGen(perm)
+        for i in stride(from: 0, to: expected.count, by: 1) {
+            let (p, last) = x.next()
+            XCTAssertEqual(p, expected[i])
+            XCTAssertEqual(last, i == expected.count - 1)
+        }
+        // hold last permutation
+        let (p, last) = x.next()
+        XCTAssertEqual(p, expected[expected.count - 1])
+        XCTAssertEqual(last, true)
+    }
+    
     func testPermGen() {
+        do {
+            var expected = [
+                [0,1,2],
+                [0,2,1],
+                [1,0,2],
+                [1,2,0],
+                [2,0,1],
+                [2,1,0],
+            ]
+            try testPermGenHelper(expected[0], expected)
+            
+            expected = [
+                [5, 5, 6, 9],
+                [5, 5, 9, 6],
+                [5, 6, 5, 9],
+                [5, 6, 9, 5],
+                [5, 9, 5, 6],
+                [5, 9, 6, 5],
+                [6, 5, 5, 9],
+                [6, 5, 9, 5],
+                [6, 9, 5, 5],
+                [9, 5, 5, 6],
+                [9, 5, 6, 5],
+                [9, 6, 5, 5],
+            ]
+            try testPermGenHelper(expected[0], expected)
+            
+            expected = [
+                [5, 6, 6, 6],
+                [6, 5, 6, 6],
+                [6, 6, 5, 6],
+                [6, 6, 6, 5],
+            ]
+            try testPermGenHelper(expected[0], expected)
+            
+            expected = [
+                [5, 5, 6, 6],
+                [5, 6, 5, 6],
+                [5, 6, 6, 5],
+                [6, 5, 5, 6],
+                [6, 5, 6, 5],
+                [6, 6, 5, 5],
+            ]
+            try testPermGenHelper(expected[0], expected)
+                
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    /*
+    func testPermGen__() {
         do {
             var x = try PermGen([0,1,2])
             var (p, last) = x.next()
@@ -35,25 +100,12 @@ final class p24Tests: XCTestCase {
             XCTAssertEqual(last, true)
             
             
-            /*
-             unc TestPermGen_012(t *testing.T) {
-                 gen := New([]int{0, 1, 2})
-                 expected := [][]int{
-                     []int{0, 1, 2},
-                     []int{0, 2, 1},
-                     []int{1, 0, 2},
-                     []int{1, 2, 0},
-                     []int{2, 0, 1},
-                     []int{2, 1, 0},
-                 }
-                 testPermGen(t, gen, expected)
-             }
-             */
             //XCTAssertEqual(x, 4179871)
         } catch {
             XCTFail()
         }
     }
+    */
     static var allTests = [
         ("testPermGen", testPermGen),
     ]
